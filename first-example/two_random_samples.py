a68a67508bce78a7a8a4795de7a4dea9b69031b9
Oliver Bakic <oliver.bakic@hotmail.com>
2025-01-23 10:53:31 +0100

    some notes

--- first-example/two_random_samples.py
+++ first-example/two_random_samples.py
@@ -1,9 +1,25 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-#? From: https://www.youtube.com/watch?v=slbZ-SLpIgg
+#? Online sources:
+#? https://www.youtube.com/watch?v=slbZ-SLpIgg
+#? https://www.youtube.com/watch?v=7TqhmX92P6U
+#?
+#? To lookup:
+#? https://www.youtube.com/watch?v=wKdmEXCvo9s 
+#?
 
 #! First simulate random outcomes between two set ranges
+"""#!--- Three steps:
+        1) Setup the predictive model
+            - identifying the dependant variable to be predicted
+            - identifying the independant variables (input risk of predictive variables)
+        2) Specify the probability distribution and assign probability weights for each
+            - define a range of likely values
+                - historical data or
+                - subjective values
+        3) Run repeated simulations to generate random values of the independant variables
+"""
 
 sims = 1000000  # Number of simulations to run
 
@@ -13,7 +29,7 @@ B = np.random.uniform(2, 6, sims)
 duration = A + B
 # print(duration)
 
-plt.figure(figsize = (3, 1.5))
+plt.figure(figsize = (10, 5))
 plt.hist(duration, density = True)
 plt.axvline(9, color = 'r')
 plt.show()
